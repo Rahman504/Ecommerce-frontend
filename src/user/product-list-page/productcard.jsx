4d5d20c53e96c8501ef42a7b2f24b164712ef592
unknown <abdulrahmanabdulazeez34@gmail.com>
2025-03-27 23:38:07 +0100

    Currency symbol changed

--- src/user/product-list-page/productcard.jsx
+++ src/user/product-list-page/productcard.jsx
@@ -9,7 +9,7 @@ const ProductCard = ({ image, name, discountedPrice, price, id }) => {
           <img src={image} alt="img" />
           <article>
             <p>{name}</p>
-            <p>$ {discountedPrice.toFixed(2)}</p>
+            <p>&#8358 {discountedPrice.toFixed(2).toLocaleString()}</p>
             <p id="price" style={{marginTop: "2px"}}>$ {price}</p>
           </article>
         </Link>   
