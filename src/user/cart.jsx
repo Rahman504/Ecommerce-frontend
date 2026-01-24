8c285057a4ee6ec0746e41e341d11bb5b2841519
unknown <abdulrahmanabdulazeez34@gmail.com>
2026-01-24 16:08:13 +0100

    Added payment integration

--- src/user/cart.jsx
+++ src/user/cart.jsx
@@ -116,12 +116,12 @@ useEffect(() => {
             <div key={item.product._id} className="cart-item">
               <div className="div1">
                 <Link to={`/products/${item.product._id}`} style={{ textDecoration: "none", color: "black" }}>
-                  <article style={{ width: "95%" }}>
+                  <article>
                     <img src={item.product.imageUrl?.[0]} alt={item.product.name} />
                     <h3 className="item-name">{item.product.name}</h3>
                   </article>
                 </Link>
-                <article style={{ width: "200px" }}>
+                <article>
                   {item.product.discount > 0 ? (
                     <>
                       <p id="price-cart">
